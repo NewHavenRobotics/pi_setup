18ed1d5688417f19668d8d6d9c6d50d2945cddf6
Ryan M <marrenryan@gmail.com>
2024-10-09 15:11:01 -0400

    added prompt customization

--- setup.sh
+++ setup.sh
@@ -1,9 +1,15 @@
 name=robot
 ip=192.168.1.101
 
+#set username and device name to $name
 usermod -l $name -d /home/$name -m robot
 groupmod -n $name robot
 hostnamectl set-hostname $name
+
+#static IP to $ip
 touch /etc/network/interfaces.d/eth0
 printf "allow-hotplug eth0\niface eth0 inet static\naddress %s\nnetwork 192.168.1.0\nnetmask 255.255.255.0\ngateway 192.168.1.1" "$ip" >> /etc/network/interfaces.d/eth0
+
+#simplify prompt
+printf"#Ryan's Simple Prompt\nPS1=\"\\e[1;32m\u\e[0m:\e[1;36m\w \e[0m\"" >> /$name/.bashrc
 exit
