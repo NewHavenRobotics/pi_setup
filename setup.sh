97ebe8b575f8a4a4b29f6ee68c0081e928a32ddf
Ryan M <marrenryan@gmail.com>
2024-10-15 00:57:01 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -9,7 +9,7 @@ hostnamectl set-hostname $name
 #static IP to $ip
 touch /etc/network/interfaces.d/eth0
 printf "allow-hotplug eth0\niface eth0 inet static\naddress %s\nnetwork 192.168.1.0\nnetmask 255.255.255.0\ngateway 192.168.1.1" "$ip" >> /etc/network/interfaces.d/eth0
-
+netplan apply
 #simplify prompt
 echo "#Ryan's Simple Prompt" >> /home/$name/.bashrc
 echo "PS1=\"\e[1;32m\u\e[0m:\e[1;36m\w\e[0m \"" >> /home/$name/.bashrc
