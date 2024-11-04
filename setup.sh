b32ef5fce6c0297bd9d6c7332667498add98162b
Ryan M <marrenryan@gmail.com>
2024-11-04 17:23:01 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -11,6 +11,7 @@ echo "PS1=\"\e[1;32m\u\e[0m:\e[1;36m\w\e[0m \"" >> /home/$name/.bashrc
 
 #static IP via netplan
 mv 00-installer-config.yaml /etc/netplan
+chmod +600 /etc/netplan/00-installer-config.yaml
 systemctl disable NetworkManager
 systemctl enable systemd-networkd
 systemctl start systemd-networkd
