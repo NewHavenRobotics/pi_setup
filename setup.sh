0cafa521d04b99a77949f6861f8abf0432d95de8
Ryan M <marrenryan@gmail.com>
2024-11-04 15:46:01 -0500

    configure static ip via netplan

--- setup.sh
+++ setup.sh
@@ -1,18 +1,19 @@
 name=robot
-ip=192.168.1.101
 
 #set username and device name to $name
 usermod -l $name -d /home/$name -m robot
 groupmod -n $name robot
 hostnamectl set-hostname $name
 
-#static IP to $ip
-touch /etc/network/interfaces.d/eth0
-printf "allow-hotplug eth0\niface eth0 inet static\naddress %s\nnetwork 192.168.1.0\nnetmask 255.255.255.0\ngateway 192.168.1.1" "$ip" >> /etc/network/interfaces.d/eth0
-netplan apply
-
 #simplify prompt
 echo "#Ryan's Simple Prompt" >> /home/$name/.bashrc
 echo "PS1=\"\e[1;32m\u\e[0m:\e[1;36m\w\e[0m \"" >> /home/$name/.bashrc
+
+#static IP via netplan
+mv 00-installer-config.yaml /etc/netplan
+systemctl disable NetworkManager
+systemctl enable systemd-networkd
+systemctl start systemd-networkd
+netplan apply
 
 reboot
