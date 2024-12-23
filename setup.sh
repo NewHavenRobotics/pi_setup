e3abe88e71e8e988f818b4ad80ea0ba8722093c4
Ryan M <marrenryan@gmail.com>
2024-12-23 16:30:37 -0500

    updated setup.sh
    fixed bug in Ryan's Simple Prompt that causes unexpected behaviors when editing long lines

--- setup.sh
+++ setup.sh
@@ -7,7 +7,7 @@ hostnamectl set-hostname $name
 
 #simplify prompt
 echo "#Ryan's Simple Prompt" >> /home/$name/.bashrc
-echo "PS1=\"\e[1;32m\u\e[0m:\e[1;36m\w\e[0m \"" >> /home/$name/.bashrc
+echo "PS1=\"\[\e[1;32m\]\u\[\e[0m\]:\[\e[1;36m\]\w\[\e[0m\] \"" >> /home/$name/.bashrc
 
 #static IP via netplan
 mv 00-installer-config.yaml /etc/netplan
